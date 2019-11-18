9befc6d3712fa128ca5e308fa611f21ea7c22b20
Nikita Temnikov <nistoc@gmail.com>
2019-11-18 23:26:33 +0300

    Add BudgetItem models

---
+++ BlazorDemo/BlazorApp1/Data/BudgetItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    public class BudgetItem
+    {
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
